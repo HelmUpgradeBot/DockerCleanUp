22b1ab7853eff6f13ef0fe9cd1b0e7bc99bac64a
Sarah Gibson <sgibson@turing.ac.uk>
2019-07-26 14:42:09 +0100

    Refactor to no longer be a class

--- DockerCleanUpBot.py
+++ DockerCleanUpBot.py
@@ -18,79 +18,6 @@ from subprocess import check_call, check_output
 # Get environment variable
 REGISTRY_NAME = os.environ.get("NAME")
 
-class dockerCleanUpBot:
-    def __init__(self):
-        self.dry_run = True if args.dry_run is None else args.dry_run
-        self.images_to_be_deleted_number = 0
-        self.images_to_be_deleted_digest = []
-
-    def cleanup(self):
-        if self.dry_run:
-            print("THIS IS A DRY RUN.  NO IMAGES WILL BE DELETED.")
-
-        self.login()
-        REPOS = self.fetch_repos()
-        self.check_manifests(REPOS)
-
-        if self.dry_run:
-            print(f"Number of images eligible for deletion: {self.images_to_be_deleted_number}")
-        else:
-            print(f"Number of images to be deleted: {self.images_to_be_deleted_number}")
-            self.delete_images()
-
-    def login(self):
-        print("--> Login to Azure")
-        check_call("az login --identity", shell=True)
-
-        print("--> Login to ACR")
-        check_call(f"az acr login -n {REGISTRY_NAME}", shell=True)
-
-    def fetch_repos(self):
-        print("--> Fetching repositories")
-        # Get the repositories in the ACR
-        output = check_output(
-            f"az acr repository list -n {REGISTRY_NAME} -o tsv",
-            shell=True
-        ).decode()
-        REPOS = output.split("\n")[:-1]
-
-        return REPOS
-
-    def check_manifests(self, REPOS):
-        # Loop over the repositories
-        for REPO in REPOS:
-            # Get the manifest for the current repository
-            output = check_output(
-                f"az acr repository show-manifests -n {REGISTRY_NAME} --repository {REPO} --orderby time_desc",
-                shell=True
-            ).decode()
-            outputs = output.replace("\n", "").replace(" ", "")[1:-1].split("},")
-
-            # Loop over the manifests for each repository
-            for j, output in enumerate(outputs):
-                if j < (len(outputs) - 1):
-                    output += "}"
-
-                # Convert the manifest to a dict and extract timestamp
-                MANIFEST = json.loads(output)
-                timestamp = pd.to_datetime(MANIFEST["timestamp"])
-
-                # Get time difference between now and the manifest timestamp
-                diff = (pd.Timestamp.now() - timestamp).days
-
-                # If an image is 90 days old or more, delete it
-                if diff >= 90:
-                    self.images_to_be_deleted_digest.append(f"{REPO}@{MAINFEST['digest']}")
-                    self.images_to_be_deleted_number += 1
-
-    def delete_images(self):
-        for image in self.images_to_be_deleted_digest:
-            print(f"--> Deleting image: {image}")
-            check_call(
-                f"az acr repository delete -n {REGISTRY_NAME} --image {image}",
-                shell=True
-            )
-
 def parse_args():
     parser = argparse.ArgumentParser()
 
@@ -102,7 +29,67 @@ def parse_args():
 
     return parser.parse_args()
 
-if __name__ == "__main__":
+def main():
     args = parse_args()
-    bot = dockerCleanUpBot()
-    bot.cleanup()
+
+    if args.dry_run:
+        print("THIS IS A DRY RUN.  NO IMAGES WILL BE DELETED.")
+
+    # Set-up
+    images_to_be_deleted_number = 0
+    images_to_be_deleted_digest = []
+
+    # Login
+    print("--> Login to Azure")
+    check_call("az login --identity -o none", shell=True)
+    print("--> Login to ACR")
+    check_call(f"az acr login -n {REGISTRY_NAME}", shell=True)
+
+    # Get the repositories in the ACR
+    print("--> Fetching repositories")
+    output = check_output(
+        f"az acr repository list -n {REGISTRY_NAME} -o tsv",
+        shell=True
+    ).decode()
+    REPOS = output.split("\n")[:-1]
+
+    # Loop over the repositories
+    for REPO in REPOS:
+        # Get the manifest for the current repository
+        output = check_output(
+            f"az acr repository show-manifests -n {REGISTRY_NAME} --repository {REPO} --orderby time_desc",
+            shell=True
+        ).decode()
+        outputs = output.replace("\n", "").replace(" ", "")[1:-1].split("},")
+
+        # Loop over the manifests for each repository
+        for j, output in enumerate(outputs):
+            if j < (len(outputs) - 1):
+                output += "}"
+
+            # Convert the manifest to a dict and extract timestamp
+            MANIFEST = json.loads(output)
+            timestamp = pd.to_datetime(MANIFEST["timestamp"])
+
+            # Get time difference between now and the manifest timestamp
+            diff = (pd.Timestamp.now() - timestamp).days
+
+            # If an image is 90 days old or more, delete it
+            if diff >= 90:
+                images_to_be_deleted_digest.append(f"{REPO}@{MAINFEST['digest']}")
+                images_to_be_deleted_number += 1
+
+    if args.dry_run:
+        print(f"Number of images eligible for deletion: {images_to_be_deleted_number}")
+    else:
+        print(f"Number of images to be deleted: {images_to_be_deleted_number}")
+
+        for image in images_to_be_deleted_digest:
+            print(f"--> Deleting image: {image}")
+            check_call(
+                f"az acr repository delete -n {REGISTRY_NAME} --image {image}",
+                shell=True
+            )
+
+if __name__ == "__main__":
+    main()
