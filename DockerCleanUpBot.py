b1744a8982312fcafa2324db2c08efbef40409bc
sgibson91 <sgibson@turing.ac.uk>
2019-08-20 13:17:41 +0100

    Refactor to class

--- DockerCleanUpBot.py
+++ DockerCleanUpBot.py
@@ -54,81 +54,109 @@ def parse_args():
 
     return parser.parse_args()
 
-def main():
-    args = parse_args()
+class DockerCleanUpBot(object):
+    def __init__(self, argsDict):
+        self.images_to_be_deleted_number = 0
+        self.images_to_be_deleted_digest = []
+        self.deleted_images_total = 0
 
-    if args.dry_run:
-        logging.info("THIS IS A DRY RUN. NO IMAGES WILL BE DELETED.")
+        # Parse arguments
+        self.name = argsDict["name"]
+        self.max_age = argsDict["max_age"]
+        self.limit = argsDict["limit"]
+        self.identity = argsDict["identity"]
+        self.dry_run = argsDict["dry_run"]
 
-    # Set-up
-    images_to_be_deleted_number = 0
-    images_to_be_deleted_digest = []
-    deleted_images_total = 0
+    def clean_up(self):
+        if self.dry_run:
+            logging.info("THIS IS A DRY RUN. NO IMAGES WILL BE DELETED.")
 
-    # Login to Azure
-    login_cmd = ["az", "login"]
-    if args.identity:
-        login_cmd.append("--identity")
-        logging.info("Login to Azure with Managed System Identity")
-    else:
-        logging.info("Login to Azure")
+        self.login()
+        self.check_acr_size()
 
-    result = run_cmd(login_cmd)
-    if result["returncode"] == 0:
-        logging.info("Successfully logged into Azure")
-    else:
-        logging.error(result["err_msg"])
-        raise AzureError(result["err_msg"])
+        if self.size < (self.limit * 1000.0):
+            logging.info(f"{self.name} size is less than: {self.limit:.2f} TB. Performing standard clean up operations.")
+            self.aggressive = False
+        else:
+            logging.info(f"{self.name} size is larger than: {self.limit:.2f} TB. Performing an aggressive clean up operation.")
+            self.aggressive = True
 
-    # Login to ACR
-    logging.info("Login to ACR")
-    acr_cmd = ["az", "acr", "login", "-n", args.name]
+        repos = self.fetch_repos()
+        self.check_manifests(repos)
 
-    result = run_cmd(acr_cmd)
-    if result["returncode"] == 0:
-        logging.info(f"Successfully logged into ACR: {args.name}")
-    else:
-        logging.error(result["err_msg"])
-        raise AzureError(result["err_msg"])
+        if self.dry_run:
+            logging.info(f"Number of images eligible for deletion: {self.images_to_be_deleted_number}")
+        else:
+            logging.info(f"Number of images to be deleted: {self.images_to_be_deleted_number}")
+            self.clean_up()
 
-    # Check the size of the ACR
-    logging.info(f"Checking size of ACR: {args.name}")
-    size_cmd = [
-        "az", "acr", "show-usage", "-n", args.name, "--query",
-        "{}".format("value[?name=='Size'].currentValue"), "-o", "tsv"
-    ]
+    def login(self):
+        # Login to Azure
+        login_cmd = ["az", "login"]
 
-    result = run_cmd(size_cmd)
-    if result["returncode"] == 0:
-        size = int(result["output"]) * 1.0e-9
-        logging.info(f"Size of {args.name}: {size:.2f} GB")
-    else:
-        logging.error(result["err_msg"])
-        raise AzureError(result["err_msg"])
+        if self.identity:
+            login_cmd.append("--identity")
+            logging.info("Login to Azure with Managed System Identity")
+        else:
+            logging.info("Login to Azure")
 
-    if size < (args.limit * 1000.0):
-        logging.info(f"{args.name} size is less than: {args.limit:.2f} TB. Performing standard clean up operations.")
-
-        # Get the repositories in the ACR
-        logging.info(f"Fetching repositories in: {args.name}")
-        list_cmd = ["az", "acr", "repository", "list", "-n", args.name, "-o", "tsv"]
-
-        result = run_cmd(list_cmd)
+        result = run_cmd(login_cmd)
         if result["returncode"] == 0:
-            logging.info(f"Successfully fetched repositories from: {args.name}")
-            repos = result["output"].split("\n")[:-1]
-            logging.info(f"Total number of repositories: {len(repos)}")
+            logging.info("Successfully logged into Azure")
         else:
             logging.error(result["err_msg"])
             raise AzureError(result["err_msg"])
 
+        # Login to ACR
+        logging.info("Login to ACR")
+        acr_cmd = ["az", "acr", "login", "-n", self.name]
+
+        result = run_cmd(acr_cmd)
+        if result["returncode"] == 0:
+            logging.info(f"Successfully logged into ACR: {self.name}")
+        else:
+            logging.error(result["err_msg"])
+            raise AzureError(result["err_msg"])
+
+    def check_acr_size(self):
+        # Check the size of the ACR
+        logging.info(f"Checking size of ACR: {self.name}")
+        size_cmd = [
+            "az", "acr", "show-usage", "-n", self.name, "--query",
+            "{}".format("value[?name=='Size'].currentValue"), "-o", "tsv"
+        ]
+
+        result = run_cmd(size_cmd)
+        if result["returncode"] == 0:
+            self.size = int(result["output"]) * 1.0e-9
+            logging.info(f"Size of {self.name}: {self.size:.2f} GB")
+        else:
+            logging.error(result["err_msg"])
+            raise AzureError(result["err_msg"])
+
+    def fetch_repos(self):
+        # Get the repositories in the ACR
+        logging.info(f"Fetching repositories in: {self.name}")
+        list_cmd = ["az", "acr", "repository", "list", "-n", self.name, "-o", "tsv"]
+
+        result = run_cmd(list_cmd)
+        if result["returncode"] == 0:
+            logging.info(f"Successfully fetched repositories from: {self.name}")
+            repos = result["output"].split("\n")[:-1]
+            logging.info(f"Total number of repositories: {len(repos)}")
+            return repos
+        else:
+            logging.error(result["err_msg"])
+            raise AzureError(result["err_msg"])
+
+    def check_manifests(self, repos):
         # Loop over the repositories
         logging.info("Checking repository manifests")
         for repo in repos:
             # Get the manifest for the current repository
             show_cmd = [
                 "az", "acr", "repository", "show-manifests", "-n",
-                args.name, "--repository", repo, "--orderby", "time_desc"
+                self.name, "--repository", repo, "--orderby", "time_desc"
             ]
 
             result = run_cmd(show_cmd)
@@ -151,37 +179,32 @@ def main():
 
                 # Get time difference between now and the manifest timestamp
                 diff = (pd.Timestamp.now() - timestamp).days
+                logging.info(f"{repo}@{manifest['digest']} is {diff} days old.")
 
-                # If an image is too old, delete it
-                if diff >= args.max_age:
-                    logging.info(f"{repo}@{manifest['digest']} is {diff} days old.")
-                    images_to_be_deleted_digest.append(f"{repo}@{manifest['digest']}")
-                    images_to_be_deleted_number += 1
+                # If an image is too old, at it to delete list
+                if diff >= self.max_age:
+                    self.images_to_be_deleted_digest.append(f"{repo}@{manifest['digest']}")
+                    self.images_to_be_deleted_number += 1
 
-        if args.dry_run:
-            logging.info(f"Number of images eligible for deletion: {images_to_be_deleted_number}")
-        else:
-            logging.info(f"Number of images to be deleted: {images_to_be_deleted_number}")
+    def delete_images(self):
+        for image in selfimages_to_be_deleted_digest:
+            logging.info(f"Deleting image: {image}")
+            del_cmd = [
+                "az", "acr", "repository", "delete", "-n", self.name,
+                "--image", image
+            ]
 
-            for image in images_to_be_deleted_digest:
-                logging.info(f"Deleting image: {image}")
-                del_cmd = [
-                    "az", "acr", "repository", "delete", "-n", args.name,
-                    "--image", image
-                ]
+            result = run_cmd(del_cmd)
+            if result["returncode"] == 0:
+                logging.info(f"Successfully deleted image: {image}")
+                self.deleted_images_total += 1
+            else:
+                logging.error(result["err_msg"])
+                raise AzureError(result["err_msg"])
 
-                result = run_cmd(del_cmd)
-                if result["returncode"] == 0:
-                    logging.info(f"Successfully deleted image: {image}")
-                    deleted_images_total += 1
-                else:
-                    logging.error(result["err_msg"])
-                    raise AzureError(result["err_msg"])
-
-            logging.info(f"Number of images deleted: {deleted_images_total}")
-
-    else:
-        logging.info(f"{args.name} size is larger than: {args.limit:.2f} TB. Performing an aggressive clean up operation.")
+        logging.info(f"Number of images deleted: {self.deleted_images_total}")
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    bot = DockerCleanUpBot(vars(args))
+    bot.clean_up()
