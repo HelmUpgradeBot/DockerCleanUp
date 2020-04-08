3a9c89df8f889bfdded5d448d9babe290f6d7c6b
Sarah Gibson <sgibson@turing.ac.uk>
2020-04-08 12:08:07 +0100

    :sparkles: Add purge option to cli

--- DockerCleanUpBot/cli.py
+++ DockerCleanUpBot/cli.py
@@ -3,14 +3,10 @@ import argparse
 
 from .DockerCleanUpBot import DockerCleanUpBot
 
-DESCRIPTION = (
-    "Script to clean old Docker images out of an Azure Container Registry"
-)
+DESCRIPTION = "Script to clean old Docker images out of an Azure Container Registry (ACR)"
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 
-parser.add_argument(
-    "name", type=str, help="Name of Azure Container Registry to clean"
-)
+parser.add_argument("name", type=str, help="Name of ACR to clean")
 
 parser.add_argument(
     "-a",
@@ -37,11 +33,20 @@ parser.add_argument(
     action="store_true",
     help="Do a dry-run, no images will be deleted.",
 )
+parser.add_argument(
+    "--purge",
+    action="store_true",
+    help="Purge all repositories within the ACR",
+)
 
 
 def main():
     """Main function"""
     args = parser.parse_args(sys.argv[1:])
+
+    if args.dry_run and args.purge:
+        raise ValueError("purge and dry-run options cannot be used together")
+
     obj = DockerCleanUpBot(vars(args))
     obj.clean_up()
 
